606bf965793bec7e78b74242b494fb12d6bc3a9c
OlgaStarchenko <o9915530@gmail.com>
2026-08-15 14:18:42 +1000

    The `react-redux` and `@reduxjs/toolkit` packages have been installed. The main Redux store has been created in the `store.js` file within the `store` folder. In `main.js`, the application is wrapped in a `Provider` connected to the `store`.

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,11 @@ import "./styles";
 
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
